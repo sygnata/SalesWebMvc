4ccde00891e75fb02bedac02b4fc269507176545
Andre <andreluiz_ferraz@hotmail.com>
2022-04-21 16:04:16 -0300

    Primeira Migration

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using SalesWebMvc.ViewModels;
+using SalesWebMvc.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
--- SalesWebMvc/Models/ViewModels/ErrorViewModel.cs
+++ SalesWebMvc/Models/ViewModels/ErrorViewModel.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace SalesWebMvc.ViewModels
+namespace SalesWebMvc.Models.ViewModels
 {
 	public class ErrorViewModel
 	{
